a2e6b62cb6fee227eaaaa20b2b328de548cb7008
devintyler <dtv206@nyu.edu>
2014-10-27 22:48:19 -0400

    Add Arrows and Intro Star

--- js/avatar.js
+++ js/avatar.js
@@ -1,55 +1,68 @@
+function change() {
+    var reset = $("#reset");
+    var choose = $("#choose");
+    var left = $("#leftArrow");
+    var right = $("#rightArrow");
+    var lText = $("#leftText");
+    var rText = $("#rightText");
+
+    if(!choose.hasClass("hidden")) {
+        choose.addClass("hidden");
+    }
+
+    if(!reset.hasClass("opaque")) {
+        reset.addClass("opaque");
+    }
+
+    if(!left.hasClass("opaque")) {
+        left.addClass("opaque");
+    }
+
+    if(!right.hasClass("opaque")) {
+        right.addClass("opaque");
+    }
+
+    if(!lText.hasClass("opaque")) {
+        lText.addClass("opaque");
+    }
+
+    if(!rText.hasClass("opaque")) {
+        rText.addClass("opaque");
+    }
+}
+
 $('.pic1').on({'click': function(){ // code for pic 1
         $('.opaque').attr('src','img/pose/Burka1.png');
 
-        var reset = $("#reset");
-
-        if(!reset.hasClass("opaque")) {
-            reset.addClass("opaque");
-        }
+        change();
     }
 });
 
 $('.pic2').on({'click': function(){ // code for pic 1
         $('.opaque').attr('src','img/pose/WarTorn1.png');
 
-        var reset = $("#reset");
-
-        if(!reset.hasClass("opaque")) {
-            reset.addClass("opaque");
-        }
+        change();
     }
 });
 
 $('.pic3').on({'click': function(){ // code for pic 1
         $('.opaque').attr('src','img/pose/Jasmine1.png');
 
-        var reset = $("#reset");
-
-        if(!reset.hasClass("opaque")) {
-            reset.addClass("opaque");
-        }
+        change();
     }
 });
 
 $('.pic4').on({'click': function(){ // code for pic 1
         $('.opaque').attr('src','img/pose/KimK1.png');
 
-        var reset = $("#reset");
-
-        if(!reset.hasClass("opaque")) {
-            reset.addClass("opaque");
-        }
+        change();
     }
 });
 
 $('.pic5').on({'click': function(){ // code for pic 1
         $('.opaque').attr('src','img/pose/AmericanGirl1.png');
 
-        var reset = $("#reset");
-
-        if(!reset.hasClass("opaque")) {
-            reset.addClass("opaque");
-        }
+        change();
     }
 });
 
